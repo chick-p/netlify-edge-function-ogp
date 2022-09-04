b0a572742a1dc677a28af9e5cec9bdcbf97d71d7
piyo <chick.p.1120@gmail.com>
2022-09-04 17:06:38 +0900

    refactor

--- netlify/edge-functions/get-meta.ts
+++ netlify/edge-functions/get-meta.ts
@@ -3,7 +3,7 @@ import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 type Meta = Record<string, string>;
 
-function extractOgp(metaElements: HTMLMetaElement[]): Meta {
+function extractOgp(metaElements: Array<HTMLMetaElement>): Meta {
   return metaElements
     .filter((element: HTMLMetaElement) => element.hasAttribute("property"))
     .reduce<Meta>((prev, element) => {
@@ -18,7 +18,10 @@ function extractOgp(metaElements: HTMLMetaElement[]): Meta {
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (request: Request, context: Context) => {
+export default async (
+  request: Request,
+  context: Context
+): Promise<Response> => {
   const url = new URL(request.url).searchParams.get("url");
   if (!url) {
     return context.json({ error: "The query parameter of url is required." });
@@ -26,11 +29,7 @@ export default async (request: Request, context: Context) => {
   const resp = await fetch(url);
   const html = await resp.text();
   const document = new DOMParser().parseFromString(html, "text/html");
-  const meta = document?.querySelectorAll("meta");
-  if (!meta) {
-    return context.json({ error: "Meta element are not found." });
-  }
-  const ogp = extractOgp([...meta]);
+  const ogp = extractOgp([...document?.querySelectorAll("meta")]);
   const response = await context.json(ogp);
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET");
